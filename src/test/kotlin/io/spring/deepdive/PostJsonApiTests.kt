472c0254a13b23a123da26221815d8b10a8dffa4
Mark Heckler <mark.heckler@gmail.com>
2017-10-30 19:14:48 -0500

    Tests

--- src/test/kotlin/io/spring/deepdive/PostJsonApiTests.kt
+++ src/test/kotlin/io/spring/deepdive/PostJsonApiTests.kt
@@ -18,43 +18,72 @@ package io.spring.deepdive
 import java.time.LocalDateTime
 
 import io.spring.deepdive.model.Post
+import io.spring.deepdive.model.PostEvent
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.springframework.web.client.HttpServerErrorException
-
-import org.springframework.web.client.getForObject
+import org.springframework.http.HttpStatus.*
+import org.springframework.http.MediaType.*
+import org.springframework.web.reactive.function.client.bodyToFlux
+import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.test.test
 
 class PostJsonApiTests : AbstractIntegrationTests() {
 
     @Test
     fun `Assert findAll JSON API is parsed correctly and contains 3 elements`() {
-        val posts = restTemplate.getForObject<List<Post>>("/api/post/")
-        assertThat(posts).hasSize(3)
+        client.get().uri("/api/post/").retrieve().bodyToFlux<Post>()
+                .test()
+                .expectNextCount(3)
+                .verifyComplete()
     }
 
     @Test
     fun `Verify findOne JSON API`() {
-        val post = restTemplate.getForObject<Post>("/api/post/reactor-bismuth-is-out")!!
-        assertThat(post.title).isEqualTo("Reactor Bismuth is out")
-        assertThat(post.headline).startsWith("It is my great pleasure to")
-        assertThat(post.content).startsWith("With the release of")
-        assertThat(post.addedAt).isEqualTo(LocalDateTime.of(2017, 9, 28, 12, 0))
-        assertThat(post.author.firstname).isEqualTo("Simon")
+        client.get().uri("/api/post/reactor-bismuth-is-out").retrieve().bodyToMono<Post>()
+                .test()
+                .consumeNextWith {
+                    assertThat(it.title).isEqualTo("Reactor Bismuth is out")
+                    assertThat(it.headline).startsWith("It is my great pleasure to")
+                    assertThat(it.content).startsWith("With the release of")
+                    assertThat(it.addedAt).isEqualTo(LocalDateTime.of(2017, 9, 28, 12, 0))
+                    assertThat(it.author).isEqualTo("simonbasle")
+                }.verifyComplete()
     }
 
     @Test
     fun `Verify findOne JSON API with Markdown converter`() {
-        val post = this.restTemplate.getForObject("/api/post/reactor-bismuth-is-out?converter=markdown", Post::class.java)!!
-        assertThat(post.title).startsWith("<p>Reactor Bismuth is out")
-        assertThat(post.headline).doesNotContain("**3.1.0.RELEASE**").contains("<strong>3.1.0.RELEASE</strong>")
-        assertThat(post.content).doesNotContain("[Spring Framework 5.0](https://spring.io/blog/2017/09/28/spring-framework-5-0-goes-ga)").contains("<a href=\"https://spring.io/blog/2017/09/28/spring-framework-5-0-goes-ga\">")
-        assertThat(post.addedAt).isEqualTo(LocalDateTime.of(2017, 9, 28, 12, 0))
-        assertThat(post.author.firstname).isEqualTo("Simon")
+        client.get().uri("/api/post/reactor-bismuth-is-out?converter=markdown").retrieve().bodyToMono<Post>()
+                .test()
+                .consumeNextWith {
+                    assertThat(it.title).startsWith("<p>Reactor Bismuth is out")
+                    assertThat(it.headline).doesNotContain("**3.1.0.RELEASE**").contains("<strong>3.1.0.RELEASE</strong>")
+                    assertThat(it.content).doesNotContain("[Spring Framework 5.0](https://spring.io/blog/2017/09/28/spring-framework-5-0-goes-ga)").contains("<a href=\"https://spring.io/blog/2017/09/28/spring-framework-5-0-goes-ga\">")
+                    assertThat(it.addedAt).isEqualTo(LocalDateTime.of(2017, 9, 28, 12, 0))
+                    assertThat(it.author).isEqualTo("simonbasle")
+                }.verifyComplete()
     }
 
     @Test
     fun `Verify findOne JSON API with invalid converter`() {
-        assertThatThrownBy { this.restTemplate.getForEntity("/api/post/reactor-bismuth-is-out?converter=foo", Post::class.java) }.isInstanceOf(HttpServerErrorException::class.java)
+        client.get().uri("/api/post/reactor-bismuth-is-out?converter=foo").exchange()
+                .test()
+                .consumeNextWith { assertThat(it.statusCode()).isEqualTo(INTERNAL_SERVER_ERROR) }
+                .verifyComplete()
+    }
+
+    @Test
+    fun `Verify post JSON API and notifications via SSE`() {
+        client.get().uri("/api/post/notifications").accept(TEXT_EVENT_STREAM).retrieve().bodyToFlux<PostEvent>()
+                .take(1)
+                .doOnSubscribe {
+                    client.post().uri("/api/post/").syncBody(Post("foo", "Foo", "foo", "foo", "mark", LocalDateTime.now())).exchange().subscribe()
+                }
+                .test()
+                .consumeNextWith {
+                    assertThat(it.slug).isEqualTo("foo")
+                    assertThat(it.title).isEqualTo("Foo")
+                }
+                .verifyComplete()
     }
 
 }
